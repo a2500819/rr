910e1020973501b4986771da8146e224ef2fa446
a2500819 <Shinelon@DESKTOP-UKPRUO2>
2018-03-18 20:51:57 +0800

    wo lai tijiao

---
+++ text.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of text
+ *
+ * @author Shinelon
+ */
+class text {
+    //put your code here
+    
+}
